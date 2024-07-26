c3b811c672eb34bd5d1e6951c38c8f907ef49706
Cesar Alvarez Marin <cesaralvarezmarin@gmail.com>
2024-07-26 20:22:13 +0200

    3.6 and 3.7

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require ('express')
 const app = express()
+const morgan = require('morgan');
 const cors = require('cors')
 
 const logger = require("./loggerMiddleware")
 
 app.use(cors())
 app.use(express.json())
-app.use(logger)
+//app.use(logger)
+
+// Define un nuevo token para capturar el cuerpo de la solicitud
+morgan.token('body', (req, res) => {
+    return JSON.stringify(req.body);
+});
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+
 
 
 let persons = [
